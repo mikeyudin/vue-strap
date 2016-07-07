b166d23b1e73843f7ef9531476e0ba180fa23c04
Mike Yudin <mikeyudin@gmail.com>
2016-07-07 19:02:05 -0400

    This commit allows users to specify a callback on the close method.  It also calls the close() method when the backdrop is clicked, instead of just calling show = false.

--- src/Modal.vue
+++ src/Modal.vue
@@ -1,18 +1,18 @@
 <template>
   <div role="dialog"
-    v-bind:class="{
+       v-bind:class="{
     'modal':true,
     'fade':effect === 'fade',
     'zoom':effect === 'zoom'
     }"
-    >
+  >
     <div v-bind:class="{'modal-dialog':true,'modal-lg':large,'modal-sm':small}" role="document"
-      v-bind:style="{width: optionalWidth}">
+         v-bind:style="{width: optionalWidth}">
       <div class="modal-content">
         <slot name="modal-header">
           <div class="modal-header">
             <button type="button" class="close" @click="close"><span>&times;</span></button>
-            <h4 class="modal-title" > 
+            <h4 class="modal-title" >
               <slot name="title">
                 {{title}}
               </slot>
@@ -34,9 +34,9 @@
 </template>
 
 <script>
-import getScrollBarWidth from './utils/getScrollBarWidth.js'
-import EventListener from './utils/EventListener.js'
-import coerceBoolean from './utils/coerceBoolean.js'
+  import getScrollBarWidth from './utils/getScrollBarWidth.js'
+  import EventListener from './utils/EventListener.js'
+  import coerceBoolean from './utils/coerceBoolean.js'
 
   export default {
     props: {
@@ -62,6 +62,10 @@ import coerceBoolean from './utils/coerceBoolean.js'
         default: null
       },
       callback: {
+        type: Function,
+        default() {}
+      },
+      closeCallback: {
         type: Function,
         default() {}
       },
@@ -100,19 +104,19 @@ import coerceBoolean from './utils/coerceBoolean.js'
           }
           if (this.backdrop) {
             this._blurModalContentEvent = EventListener.listen(this.$el, 'click', (e)=> {
-              if (e.target === el) this.show = false
-            })
+                      if (e.target === el) this.close()
+          })
           }
         } else {
           if (this._blurModalContentEvent) this._blurModalContentEvent.remove()
-          el.classList.remove('in')
-          setTimeout(()=> {
-            el.style.display = 'none'
-            body.classList.remove('modal-open')
-            body.style.paddingRight = '0'
-          }, 300)
-        }
-      }, { immediate: true })
+      el.classList.remove('in')
+      setTimeout(()=> {
+        el.style.display = 'none'
+      body.classList.remove('modal-open')
+      body.style.paddingRight = '0'
+    }, 300)
+    }
+    }, { immediate: true })
     },
     computed: {
       optionalWidth: function() {
@@ -127,18 +131,20 @@ import coerceBoolean from './utils/coerceBoolean.js'
     methods: {
       close() {
         this.show = false
+
+        this.closeCallback()
       }
     }
   }
 </script>
 <style>
-.modal {
-  transition: all 0.3s ease;
-}
-.modal.in {
-  background-color: rgba(0,0,0,0.5);
-}
-.modal.zoom .modal-dialog {
+  .modal {
+    transition: all 0.3s ease;
+  }
+  .modal.in {
+    background-color: rgba(0,0,0,0.5);
+  }
+  .modal.zoom .modal-dialog {
     -webkit-transform: scale(0.1);
     -moz-transform: scale(0.1);
     -ms-transform: scale(0.1);
@@ -148,8 +154,8 @@ import coerceBoolean from './utils/coerceBoolean.js'
     -webkit-transition: all 0.3s;
     -moz-transition: all 0.3s;
     transition: all 0.3s;
-}
-.modal.zoom.in .modal-dialog {
+  }
+  .modal.zoom.in .modal-dialog {
     -webkit-transform: scale(1);
     -moz-transform: scale(1);
     -ms-transform: scale(1);
@@ -157,5 +163,5 @@ import coerceBoolean from './utils/coerceBoolean.js'
     -webkit-transform: translate3d(0, -300px, 0);
     transform: translate3d(0, -300px, 0);
     opacity: 1;
-}
+  }
 </style>
